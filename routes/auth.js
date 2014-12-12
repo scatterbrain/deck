44da2159f7a21b5a5176f6a65a876b3324810466
Mikko Hämäläinen <mikko.hamalainen@greyarealabs.com>
2014-12-12 15:56:53 +0200

    Auth success redirect

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ module.exports = function(passport) {
     router.get('/facebook', passport.authenticate('facebook', { scope : 'email' }) );
 
     // handle the callback after facebook has authenticated the user
-    router.get('/facebook/callback', passport.authenticate('facebook', { successRedirect : '/profile', failureRedirect : '/' } ));
+    router.get('/facebook/callback', passport.authenticate('facebook', { successRedirect : '/', failureRedirect : '/' } ));
 
     // route for logging out
     router.get('/logout', function(req, res) {
